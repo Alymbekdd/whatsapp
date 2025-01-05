df81f6168297a247dd9da95c15d4f714cccb04c2
Alymbekdd <usekowalymbek2002@gmail.com>
2025-01-05 17:18:32 +0600

    Update todo.js id

--- src/pages/api/todo.js
+++ src/pages/api/todo.js
@@ -8,18 +8,8 @@ export default function handler(req, res) {
         const newTodo = {id: Math.round(Math.random()*1000), task, completed: false};
         todos.push(newTodo);
         res.status(201).json(newTodo);
-    }else if(req.method === 'PUT') {
-        const {id} = req.query;
-        const {task, completed} =req.body;
-        const index = todos.findIndex(todo => todo.id === parseInt(id));
-        todos[index] = {id: parseInt(id), task, completed};
-        res.status(200).json(todos[index]);
-    }else if(req.method === 'DELETE') {
-      const {id} = req.query;
-      todos = todos.filter(todo => todo.id !== parseInt(is));
-      res.status(200).json({message: 'Todo deleted successfully'});
     }else {
-        res.setHeader('Allow', ['GET', 'POST', 'PUT', 'DELETE']);
+        res.setHeader('Allow', ['GET', 'POST']);
         res.status(405).end(`Method ${req.method} Not Allowed`);
     }
 }
